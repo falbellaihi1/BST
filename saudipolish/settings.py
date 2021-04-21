b63b3474973a0f8c5474c0f3b4e1ea9eec6f91b1
FAISAL ALBELLAIHI <falbellaihi123@gmail.com>
2021-04-21 18:44:19 +0300

    updated requirements.txt

--- saudipolish/settings.py
+++ saudipolish/settings.py
@@ -15,17 +15,16 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'#os.environ.get('SAUDI_POLISH_SECRET_KEY')
+SECRET_KEY = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'  #
+# os.environ.get('SAUDI_POLISH_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -45,12 +44,11 @@ INSTALLED_APPS = [
     'crispy_forms',
     'widget_tweaks',
     'django_gravatar',
+    'rest_framework',
 
+]
 
-
-    ]
-
-AUTH_USER_MODEL ='accounts.MyUser'
+AUTH_USER_MODEL = 'accounts.MyUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -60,7 +58,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
 
 ]
 INTERNAL_IPS = [
@@ -91,13 +88,12 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.transaction.TransactionMiddleware',
 )
 
-
-ROOT_URLCONF = 'saudipolish.urls' 
+ROOT_URLCONF = 'saudipolish.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [(os.path.join(BASE_DIR, 'templates')),],
+        'DIRS': [(os.path.join(BASE_DIR, 'templates')), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,10 +109,8 @@ LOGIN_REDIRECT_URL = "index"
 LOGOUT_REDIRECT_URL = 'index'
 WSGI_APPLICATION = 'saudipolish.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 
 
 DATABASES = {
@@ -137,25 +131,27 @@ DATABASES = {
     }
 }"""
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
@@ -171,13 +167,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-DEFAULT_CURRENCY="USD"
-CURRENCIES=["USD","SAR"]
-#LC_NUMERIC ="en_US.utf8"
-#LC_ALL ="en_US.utf8"
-#LANG = "en_US.utf8"
+DEFAULT_CURRENCY = "USD"
+CURRENCIES = ["USD", "SAR"]
+# LC_NUMERIC ="en_US.utf8"
+# LC_ALL ="en_US.utf8"
+# LANG = "en_US.utf8"
