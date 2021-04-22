6cb8bf067833e32297f5796bc958e67610a7d231
FAISAL ALBELLAIHI <falbellaihi123@gmail.com>
2021-04-22 04:25:42 +0300

    updated settings

--- saudipolish/settings.py
+++ saudipolish/settings.py
@@ -171,12 +171,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_CURRENCY = "USD"
 CURRENCIES = ["USD", "SAR"]
 # LC_NUMERIC ="en_US.utf8"
 # LC_ALL ="en_US.utf8"
 # LANG = "en_US.utf8"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, STATIC_URL),
-]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
