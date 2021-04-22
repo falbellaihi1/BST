1fc30e1f640a0b65d86e542086d26d60c7b2006d
FAISAL ALBELLAIHI <falbellaihi123@gmail.com>
2021-04-22 04:15:44 +0300

    restored statics

--- saudipolish/settings.py
+++ saudipolish/settings.py
@@ -170,13 +170,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 DEFAULT_CURRENCY = "USD"
 CURRENCIES = ["USD", "SAR"]
 # LC_NUMERIC ="en_US.utf8"
 # LC_ALL ="en_US.utf8"
 # LANG = "en_US.utf8"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
