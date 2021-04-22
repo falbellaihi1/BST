08d9d573f456bcb1d0984e8c543fc45a87ba4f25
FAISAL ALBELLAIHI <falbellaihi123@gmail.com>
2021-04-22 04:20:32 +0300

    updated settings

--- saudipolish/settings.py
+++ saudipolish/settings.py
@@ -170,7 +170,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 DEFAULT_CURRENCY = "USD"
 CURRENCIES = ["USD", "SAR"]
